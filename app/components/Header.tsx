42b34b0444395498d02c17e5f277af018d821619
LeoCarrijo <lh83712@gmail.com>
2024-12-25 11:25:23 -0300

    :sparkles: feat: Finalização do modelo do cabeçalho

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+import { ChevronDown, House, MessageSquare, Plane } from "lucide-react";
 
 export default function Header() {
     return (
@@ -19,59 +17,45 @@ export default function Header() {
             </Link>
 
             <nav>
-                <ul className="flex gap-4">
-                    <li>
+                <ul className="flex gap-4 text-white">
+                    <li className="cursor-pointer">
                         <Popover>
                             <PopoverTrigger asChild>
-                                <Button variant="outline">Stays</Button>
+                                <span className="flex justify-center items-center">Stays <ChevronDown /></span>
                             </PopoverTrigger>
-                            <PopoverContent className="">
-                                <div className="grid gap-4">
-                                    <div className="space-y-2">
-                                        <h4 className="font-medium leading-none">Dimensions</h4>
-                                        <p className="text-sm text-muted-foreground">
-                                            Set the dimensions for the layer.
-                                        </p>
-                                    </div>
-                                    <div className="grid gap-2">
-                                        <div className="grid grid-cols-3 items-center gap-4">
-                                            <Label htmlFor="width">Width</Label>
-                                            <Input
-                                                id="width"
-                                                defaultValue="100%"
-                                                className="col-span-2 h-8"
-                                            />
-                                        </div>
-                                        <div className="grid grid-cols-3 items-center gap-4">
-                                            <Label htmlFor="maxWidth">Max. width</Label>
-                                            <Input
-                                                id="maxWidth"
-                                                defaultValue="300px"
-                                                className="col-span-2 h-8"
-                                            />
-                                        </div>
-                                        <div className="grid grid-cols-3 items-center gap-4">
-                                            <Label htmlFor="height">Height</Label>
-                                            <Input
-                                                id="height"
-                                                defaultValue="25px"
-                                                className="col-span-2 h-8"
-                                            />
-                                        </div>
-                                        <div className="grid grid-cols-3 items-center gap-4">
-                                            <Label htmlFor="maxHeight">Max. height</Label>
-                                            <Input
-                                                id="maxHeight"
-                                                defaultValue="none"
-                                                className="col-span-2 h-8"
-                                            />
-                                        </div>
-                                    </div>
+                            <PopoverContent className="flex flex-col gap-4 w-auto pb-14">
+                                <Link href="/" className="flex items-center justify-center px-2 gap-4 border-2 border-blue-500 rounded-lg p-2 hover:bg-blue-200 duration-100 active:bg-blue-400">
+                                    <House color="blue" width={70} height={55} className="bg-gray-200 justify-self-end p-3 rounded-lg border-blue-300 shadow-lg shadow-black-300" />
+                                    <aside className="w-full">
+                                        <h4 className="text-blue-600 font-medium">Book a Stay</h4>
+                                        <p className="text-ellipsis truncate text-sm">Get a better understanting of our traffic</p>
+                                    </aside>
+                                </Link>
+                                <Link href="/" className="flex items-center justify-center px-2 gap-4 border-2 border-blue-500 rounded-lg p-2 hover:bg-blue-200 duration-100 active:bg-blue-400">
+                                    <Plane color="blue" width={70} height={55} className="bg-gray-200 justify-self-end p-3 rounded-lg border-blue-300 shadow-lg shadow-black-300" />
+                                    <aside className="w-full">
+                                        <h4 className="text-blue-600 font-medium">Book a Flight</h4>
+                                        <p className="text-ellipsis truncate text-sm">Speak directly to your customers</p>
+                                    </aside>
+                                </Link>
+                                <Link href="/" className="flex items-center justify-center px-2 gap-4 border-2 border-blue-500 rounded-lg p-2 hover:bg-blue-200 duration-100 active:bg-blue-400">
+                                    <MessageSquare color="blue" width={70} height={55} className="bg-gray-200 justify-self-end p-3 rounded-lg border-blue-300 shadow-lg shadow-black-300" />
+                                    <aside className="w-full">
+                                        <h4 className="text-blue-600 font-medium">Contact out Support Team</h4>
+                                        <p className="text-ellipsis truncate text-sm">Your customers data will be safe and secure </p>
+                                    </aside>
+                                </Link>
+                                <div className="absolute bottom-0 left-0 w-full h-10 rounded-b-lg flex justify-center items-center border-collapse">
+                                    <Link href="/" className="flex justify-center items-center w-full h-full border-t-2 border-blue-500 hover:bg-blue-200 duration-100 active:bg-blue-400">
+                                        <h4 className="text-blue-600 font-medium">See Demo Booking</h4>
+                                    </Link>
+                                    <Link href="/" className="w-full flex justify-center items-center h-full border-l-2 border-t-2 border-blue-500 hover:bg-blue-200 duration-100 active:bg-blue-400">
+                                        <h4 className="text-blue-600 font-medium">Contact Support</h4>
+                                    </Link>
                                 </div>
                             </PopoverContent>
                         </Popover>
                     </li>
-                    <li>Stays</li>
                     <li>Flights</li>
                     <li>Car rentals</li>
                     <li>Attractions</li>

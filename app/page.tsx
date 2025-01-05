0ed0f8233f093e243197555ee25eaf8cc0423cfa
LeoCarrijo <lh83712@gmail.com>
2025-01-05 13:09:11 -0300

    :sparkles: feat: Criaçao do cabeçalho do main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1>Booking.com clone</h1>
+    <main className="bg-blue-800">
+      <section className="max-w-7xl mx-auto pb-4 px-3 lg:px-0">
+        <h1 className="text-white font-bold text-5xl pb-2">Find your next Stay</h1>
+        <h3 className="text-white">Search low prices on hotels, homes and much more...</h3>
+      </section>
+    </main>
   );
 }

542cec636f567e948d35ab39b9ac579b8cb17793
LeoCarrijo <lh83712@gmail.com>
2025-01-05 13:10:08 -0300

    :card_file_box: raw: Importaçao de dados dummy

---
+++ app/data/tranding.ts
@@ -0,0 +1,37 @@
+export const tranding_data = [
+	{
+		id: 1,
+		src: "https://cf.bstatic.com/xdata/images/city/600x600/653646.jpg?k=0a8eb3999748f5b12b29f6bd5492a12b7f15a37535e572ea91403098d1588d38&o=",
+		title: "São Paulo",
+		location: "Brazil",
+		description: "45 Deals",
+	},
+	{
+		id: 2,
+		src: "https://cf.bstatic.com/xdata/images/city/600x600/653641.jpg?k=29b8706ee4bff7b870ca35a698c5ac4be003b4122b00035a9d4a572d3101b1fe&o=",
+		title: "Rio de Janeiro",
+		location: "Brazil",
+		description: "37 Deals",
+	},
+	{
+		id: 3,
+		src: "https://cf.bstatic.com/xdata/images/city/600x600/653430.jpg?k=0d90e6eabe2d8a5efc49b01c77e90407a1ce508bb4c963318689d4ce8b753020&o=",
+		title: "Belo Horizonte",
+		location: "Brazil",
+		description: "26 Deals",
+	},
+	{
+		id: 4,
+		src: "https://cf.bstatic.com/xdata/images/city/600x600/653459.jpg?k=57c5c21e650b8e0888c5906143c2655c412a2e64796337d87a687c68daabed2d&o=",
+		title: "Curitiba",
+		location: "Brazil",
+		description: "34 Deals",
+	},
+	{
+		id: 5,
+		src: "https://cf.bstatic.com/xdata/images/city/600x600/653655.jpg?k=1cb0973d5385825a0bbbf8361acdd48af2279ed3f4dca409263a618dc6c3860c&o=",
+		title: "Salvador",
+		location: "Brazil",
+		description: "29 Deals",
+	},
+];

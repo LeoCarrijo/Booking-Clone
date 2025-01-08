96c630806ae154ceb2eff2e510ddb14f08ad5e66
LeoCarrijo <lh83712@gmail.com>
2025-01-08 06:50:40 -0300

    :sparkles: feat: Desenvolvendo componente "SearchForm"

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,4 +1,17 @@
 import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const formSchema = z.object({
     location: z.string().min(2).max(50),
@@ -15,7 +28,52 @@ export const formSchema = z.object({
 });
 
 export default function SearchForm() {
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            location: "",
+            dates: {
+                from: undefined,
+                to: undefined,
+            },
+            adults: "1",
+            children: "0",
+            rooms: "1",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <h1>Search Form</h1>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                <FormField
+                    control={form.control}
+                    name="location"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Location
+                                {/* Ícone de cama */}
+                            </FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
     )
 }

d3d29217c0d88a234496cb26ed674c0892b79aa0
LeoCarrijo <lh83712@gmail.com>
2025-01-14 12:34:10 -0300

    :sparkles: feat: Mecanismo de Pesquisa

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as z from 'zod';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -39,6 +40,8 @@ export const formSchema = z.object({
 });
 
 export default function SearchForm() {
+    const router = useRouter()
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -55,6 +58,26 @@ export default function SearchForm() {
 
     function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values)
+
+        const checkin_monthday = values.dates.from.getDate().toString()
+        const checkin_month = (values.dates.from.getMonth() + 1).toString()
+        const checkin_year = values.dates.from.getFullYear().toString()
+        const checkout_monthday = values.dates.to.getDate().toString()
+        const checkout_month = (values.dates.to.getMonth() + 1).toString()
+        const checkout_year = values.dates.to.getFullYear().toString()
+
+        const checkin = `${checkin_year}-${checkin_month}-${checkin_monthday}`
+        const checkout = `${checkout_year}-${checkout_month}-${checkout_monthday}`
+
+        const url = new URL("https://www.booking.com/searchresults.html")
+        url.searchParams.set("ss", values.location)
+        url.searchParams.set("group_adults", values.adults)
+        url.searchParams.set("group_children", values.children)
+        url.searchParams.set("no_rooms", values.rooms)
+        url.searchParams.set("checkin", checkin)
+        url.searchParams.set("checkout", checkout)
+
+        router.push(`/search?url=${url.href}`)
     }
 
     return (

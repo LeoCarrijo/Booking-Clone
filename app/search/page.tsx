ddad62d9e0443c1fa374822712aae7887530ad18
LeoCarrijo <lh83712@gmail.com>
2025-01-26 13:27:25 -0300

    :sparkles: feat: Exibindo as imagens

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,38 @@
+import Image from "next/image"
+
 async function fetchTitles() {
     const response = await fetch("http://localhost:3000/api/getTitles")
     const data = await response.json()
     return data
 }
 
+async function fetchImages() {
+    const response = await fetch("http://localhost:3000/api/getImages")
+    const data = await response.json()
+    return data
+}
+
 export default async function SearchPage() {
     const titles: string[] = await fetchTitles()
+    const images: string[] = await fetchImages()
 
     return (
         <div>
-            <h1>Search Page</h1>
             <ul>
                 {titles.map((title) => {
                     return <li key={title}>{title}</li>
+                })}
+            </ul>
+            <ul className="flex">
+                {images.map((image) => {
+                    return <li key={image}>
+                        <Image
+                            src={image}
+                            width={100}
+                            height={100}
+                            alt="Image"
+                        />
+                    </li>
                 })}
             </ul>
         </div>
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "stardewvalleywiki.com",
+				hostname: "cf.bstatic.com",
 				pathname: "/**",
 			},
 		],

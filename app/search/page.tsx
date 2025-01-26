c0fbff474ddd66f2be58af982bfad2132d83ea4f
LeoCarrijo <lh83712@gmail.com>
2025-01-26 13:10:10 -0300

    :sparkles: feat: Exibindo Títulos na página

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,20 @@
-export default function SearchPage() {
-    const url = "https://www.booking.com/searchresults.html?ss=Salvador&group_adults=3&group_children=0&no_rooms=1&checkin=2025-2-11&checkout=2025-2-13";
-    const apiUrl = `http://localhost:3000/api?url=${encodeURIComponent(url)}`;
+async function fetchTitles() {
+    const response = await fetch("http://localhost:3000/api/getTitles")
+    const data = await response.json()
+    return data
+}
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
+export default async function SearchPage() {
+    const titles: string[] = await fetchTitles()
 
     return (
         <div>
             <h1>Search Page</h1>
+            <ul>
+                {titles.map((title) => {
+                    return <li key={title}>{title}</li>
+                })}
+            </ul>
         </div>
     )
 }
